b53637b4bdddfefa604e921697c8af1972bafab6
joker <>
2021-03-08 16:05:16 +0800

    first commit

---
+++ js/language.js
@@ -0,0 +1,533 @@
+var indexhtml = {
+    sncoll:{
+        'zh-cn':'收集列表',
+        'en-us':'Collections'
+    },
+    connect:{
+        'zh-cn':'连接',
+        'en-us':'Connect'
+    },
+    conwallet:{
+        'zh-cn':'连接钱包',
+        'en-us':'Connect Wallet'
+    },
+    newnft:{
+        'zh-cn':'申请上架',
+        'en-us':'New Collection'
+    },
+    add:{
+        'zh-cn':'添加',
+        'en-us':'Add'
+    },
+    contact:{
+        'zh-cn':'联系',
+        'en-us':'Contact'
+    },
+    
+    farmimg:{
+        'zh-cn':'农场',
+        'en-us':'Farming'
+    },
+    about:{
+        'zh-cn':'关于',
+        'en-us':'About'
+    },
+    type:{
+        'zh-cn':'类型',
+        'en-us':'Type'
+    },
+    sortreclist:{
+        'zh-cn':'按最新发布',
+        'en-us':'Recently Listed'
+    },
+    sortexpsoon:{
+        'zh-cn':'按即将过期',
+        'en-us':'Expiring Soon'
+    },
+    sortlowprice:{
+        'zh-cn':'按最低价格',
+        'en-us':'Lowest Price'
+    },
+    sorthighprice:{
+        'zh-cn':'按最高价格',
+        'en-us':'Highest Price'
+    },
+    level:{
+        'zh-cn':'等级',
+        'en-us':'level'
+    },
+    fixprice:{
+        'zh-cn':'限价',
+        'en-us':'Fixed Price'
+    },
+    onauction:{
+        'zh-cn':'拍卖',
+        'en-us':'On Auction'
+    },
+    fixpricek:{
+        'zh-cn':'　　限价　　',
+        'en-us':'Fixed Price'
+    },
+    onauctionk:{
+        'zh-cn':'　　拍卖　　',
+        'en-us':'On Auction'
+    },
+    orinstall:{
+        'zh-cn':'或者安装',
+        'en-us':'Or install'
+    },
+    install:{
+        'zh-cn':'安装',
+        'en-us':'Install'
+    },
+    inst1:{
+        'zh-cn':'请安装',
+        'en-us':'Please install'
+    },
+    inst2:{
+        'zh-cn':'来玩!',
+        'en-us':'to use!'
+    },
+    reflink:{
+        'zh-cn':'邀请链接',
+        'en-us':'Refer Link'
+    },
+    copy:{
+        'zh-cn':'复制',
+        'en-us':'Copy'
+    },
+    regis:{
+        'zh-cn':'注册',
+        'en-us':'Register'
+    },
+    withdraw:{
+        'zh-cn':'提现',
+        'en-us':'Withdraw'
+    },
+   
+    close:{
+        'zh-cn':'关闭',
+        'en-us':'Close'
+    },
+
+    reload:{
+        'zh-cn':'刷新',
+        'en-us':'Reload'
+    },
+    share:{
+        'zh-cn':'分享',
+        'en-us':'Share'
+    },
+    ownedby:{
+        'zh-cn':'拥有者:',
+        'en-us':'Owned by:'
+    },
+    auction:{
+        'zh-cn':'拍卖',
+        'en-us':'Auction'
+    },
+    ends:{
+        'zh-cn':'结束',
+        'en-us':'Ends'
+    },
+    curprice:{
+        'zh-cn':'当前价格',
+        'en-us':'Current Price'
+    },
+    minbid:{
+        'zh-cn':'最小出价',
+        'en-us':'Minimum Bid'
+    },
+    buynow:{
+        'zh-cn':'现在购买',
+        'en-us':'Buy Now'
+    },
+    bid:{
+        'zh-cn':'出价',
+        'en-us':'Bid'
+    },
+    bidnow:{
+        'zh-cn':'现在出价',
+        'en-us':'Bid Now'
+    },
+    rulauction:{
+        'zh-cn':'裁决拍卖',
+        'en-us':'Ruling Auction'
+    },
+    auctip1:{
+        'zh-cn':'当前拍卖有',
+        'en-us':'The current auction has'
+    },
+    auctip2:{
+        'zh-cn':'次竞拍',
+        'en-us':'bid(s)'
+    },
+    sell:{
+        'zh-cn':'出售',
+        'en-us':'Sell'
+    },
+    udprice:{
+        'zh-cn':'更新价格',
+        'en-us':'Update Price'
+    },
+    cancel:{
+        'zh-cn':'取消',
+        'en-us':'Cancel'
+    },
+    accept:{
+        'zh-cn':'接受',
+        'en-us':'Accept'
+    },
+    cancel1:{
+        'zh-cn':'下架',
+        'en-us':'Cancel'
+    },
+    attrs:{
+        'zh-cn':'属性',
+        'en-us':'Attributes'
+    },
+    bids:{
+        'zh-cn':'出价',
+        'en-us':'Bids'
+    },
+    tradehis:{
+        'zh-cn':'交易记录',
+        'en-us':'Trade History'
+    },
+    mynft:{
+        'zh-cn':'我的NFT(s)',
+        'en-us':'My NFT(s)'
+    },
+    mybid:{
+        'zh-cn':'我的出价',
+        'en-us':'My Bid(s)'
+    },
+    received:{
+        'zh-cn':'已接收',
+        'en-us':'Received'
+    },
+    sent:{
+        'zh-cn':'已发送',
+        'en-us':'Sent'
+    },
+    receive:{
+        'zh-cn':'　接收　',
+        'en-us':'Receives'
+    },
+    send:{
+        'zh-cn':'　发送　',
+        'en-us':'Sends'
+    },
+    duration:{
+        'zh-cn':'期限',
+        'en-us':'duration'
+    },
+    days:{
+        'zh-cn':'天',
+        'en-us':'days'
+    },
+    lastsold:{
+        'zh-cn':'上次出售',
+        'en-us':'Last Sold'
+    },
+    
+    wfbc:{
+        'zh-cn':'等待区块链确认',
+        'en-us':'WAITING FOR BLOCKCHAIN CONFIRMATION'
+    },
+    
+  }
+  
+
+
+
+  var appjs ={
+    version:{
+        'zh-cn':'1.05',
+        'en-us':'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890.,/#'
+    },
+    sellconfirm:{
+        'zh-cn':'你确认接受这个报价，并出售该NFT吗？',
+        'en-us':'Are you sure to accept this bid and sell the NFT?'
+    },
+    add:{
+        'zh-cn':'添加',
+        'en-us':'Add'
+    },
+    cancel:{
+        'zh-cn':'取消',
+        'en-us':'Cancel'
+    },
+    accept:{
+        'zh-cn':'接受',
+        'en-us':'Accept'
+    },
+    nitem:{
+        'zh-cn':'新订单',
+        'en-us':'New Item'
+    },
+    init:{
+        'zh-cn':'初始',
+        'en-us':'Init '
+    },
+    price:{
+        'zh-cn':'价格',
+        'en-us':'Price'
+    },
+    none:{
+        'zh-cn':'暂无',
+        'en-us':'None'
+    },
+    totalfee:{
+        'zh-cn':'手续费',
+        'en-us':'Total Fees'
+    },
+    loadmore:{
+        'zh-cn':'加载更新',
+        'en-us':'Load More'
+    },
+    lastprice:{
+        'zh-cn':'上次出售价格',
+        'en-us':'Last Sold Price'
+    },
+    
+    notradehis:{
+        'zh-cn':'暂无交易记录',
+        'en-us':'No trade history yet!'
+    },
+    noitem:{
+        'zh-cn':'暂无交易订单',
+        'en-us':'No item yet!'
+    },
+    nobid:{
+        'zh-cn':'暂无出价记录',
+        'en-us':'No bid yet!'
+    },
+    auction:{
+        'zh-cn':'拍卖',
+        'en-us':'Auction'
+    },
+    event:{
+        'zh-cn':'事件',
+        'en-us':'Event'
+    },
+    price:{
+        'zh-cn':'价格',
+        'en-us':'Price'
+    },
+    from:{
+        'zh-cn':'来自',
+        'en-us':'From'
+    },
+    time:{
+        'zh-cn':'时间',
+        'en-us':'Time'
+    },
+    ends:{
+        'zh-cn':'结束',
+        'en-us':'Ends'
+    },
+    dealt:{
+        'zh-cn':' 成交',
+        'en-us':' Dealt'
+    },
+    canaref:{
+        'zh-cn':' 取消并退款',
+        'en-us':' Cancel and refund'
+    },
+    udprice:{
+        'zh-cn':'更新价格',
+        'en-us':'Update Price'
+    },
+    update:{
+        'zh-cn':'更新',
+        'en-us':'Update'
+    },
+    bid:{
+        'zh-cn':'出价',
+        'en-us':'Bid'
+    },
+    bidsucc:{
+        'zh-cn':'出价成功！',
+        'en-us':'Bid success!'
+    },
+    priceerr:{
+        'zh-cn':'价格输入有误！',
+        'en-us':'Price amount error!'
+    },
+    lowprice:{
+        'zh-cn':'出价失败，价格必须高于当前价！',
+        'en-us':'Bid fail, must be greater than current price！'
+    },
+    addsucc:{
+        'zh-cn':'上架成功! ',
+        'en-us':'List success !'
+    },
+    syncfail:{
+        'zh-cn':'同步失败！',
+        'en-us':'Sync fail ！'
+    },
+    udsucc:{
+        'zh-cn':'更新成功！',
+        'en-us':'Update success !'
+    },
+    offsaling:{
+        'zh-cn':'下架中...',
+        'en-us':'Cancelling...'
+    },
+    offsalsucc:{
+        'zh-cn':'下架成功！',
+        'en-us':'Cancel success !'
+    },
+    canlling:{
+        'zh-cn':'取消中...',
+        'en-us':'Cancelling...'
+    },
+    cansucc:{
+        'zh-cn':'取消成功！',
+        'en-us':'Cancel success !'
+    },
+    synching:{
+        'zh-cn':'同步中...',
+        'en-us':'Synching...'
+    },
+    syncsucc:{
+        'zh-cn':'同步成功！',
+        'en-us':'Sync success!'
+    },
+    syncfail2:{
+        'zh-cn':'未同步成功，请刷新!',
+        'en-us':'Sync fail, please refresh!'
+    },
+
+    authing:{
+        'zh-cn':'申请权限中...',
+        'en-us':'Authorizing...'
+    },
+    buying:{
+        'zh-cn':'购买中...',
+        'en-us':'Purchasing...'
+    },
+    coplting:{
+        'zh-cn':'正在完成中...',
+        'en-us':'Completing...'
+    },
+    accptsucc:{
+        'zh-cn':'接受成功！',
+        'en-us':'Accept success !'
+    },
+    rulauction:{
+        'zh-cn':'裁决拍卖',
+        'en-us':'Ruling Auction'
+    },
+    ruling:{
+        'zh-cn':'拍卖裁决中...',
+        'en-us':'Ruling Auction...'
+    },
+    rulsucc:{
+        'zh-cn':'拍卖裁决成功！',
+        'en-us':'Ruling auction success !'
+    },
+    copysucc:{
+        'zh-cn':'复制链接成功！',
+        'en-us':'Copy success!'
+    },
+    verified:{
+        'zh-cn':'已验证',
+        'en-us':'Verified'
+    },
+    unverified:{
+        'zh-cn':'未验证',
+        'en-us':'Unverified'
+    },
+    contract:{
+        'zh-cn':'合约',
+        'en-us':' Contract'
+    },
+    auctcancel:{
+        'zh-cn':' 拍卖取消，请',
+        'en-us':'  Auction cancelled, please'
+    },
+    refund:{
+        'zh-cn':'退款',
+        'en-us':'refund'
+    },
+    waitauctend:{
+        'zh-cn':' 竞拍成功！等待拍卖结束...',
+        'en-us':' Successful bidding,Waiting for the auction to end...'
+    },
+    auctsucc:{
+        'zh-cn':' 竞拍成功！前往 ',
+        'en-us':' Successful bidding,go to '
+    },
+
+    
+  }
+
+
+
+
+  
+  var i18nData={
+    indexhtml:indexhtml,
+    appjs:appjs
+  }
+  var I18N = {
+    DATA : i18nData,
+    language: 'zh-cn',
+    setLanguage: function(language){
+        this.language = language;
+    }
+
+    ,parse: function(str, pageName){
+        var _this = this;
+        if(!pageName){
+            console.error('I18N解析失败，原因pageName='+pageName);
+            return;
+        }
+        if(!_this.language){
+            console.error('I18N解析失败，原因language='+_this.language);
+            return;
+        }
+        var key= '',
+        parseStr = '';
+        parseStr = str.replace(/{{i18n-(.+?)}}/g, function(t){
+            key = t.slice(7, t.length - 2);
+            try{
+                return _this.DATA[pageName][key][_this.language] || '';
+            }catch (e){
+                console.warn('language not found, key:'+ key +'----val'+ _this.language );
+                return '';
+            }
+        });
+        return parseStr;
+    }
+   
+    ,getText: function(pageName, key, v1, v2, v3){
+        var _this = this;
+        var intrusion = [];
+        if(arguments.length == 3 && typeof(arguments[2]) == 'object'){
+            intrusion = arguments[2];
+        }
+        else if(arguments.length > 2){
+            for(var i=2; i< arguments.length; i++){
+            intrusion.push(arguments[i]);
+            }
+        }
+        var _lg = '';
+        try{
+            _lg = _this.DATA[pageName][key][_this.language] || '';
+            if(!intrusion || intrusion.length == 0){
+                return _lg;
+            }
+            _lg = _lg.replace(/{d+}/g, function( word ){
+                return intrusion[word.match(/d+/)];
+            });
+            return _lg;
+        }catch (e){
+            console.warn('language not found, key:'+ key +'----val'+ _this.language );
+            return '';
+        }
+    }
+  };
+  
